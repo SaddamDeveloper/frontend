3672f322d9b21fc34bcf598f3983678fa1747d9f
Saddam Hussain <36893768+SaddamDeveloper@users.noreply.github.com>
2023-06-01 18:04:54 +0530

    sd

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,6 @@
 import { store } from "quasar/wrappers";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import {piniaPluginPersistedstate} from "pinia-plugin-persistedstate";
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation;
